47b8d430b24ac816df319cf09b2047b238d54bd3
Batı Gökovalı <batigokovali@icloud.com>
2023-02-01 19:10:59 +0300

    update

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,5 +1,5 @@
-import {Row} from 'react-bootstrap';
-import { Component} from 'react'
+import { Row } from 'react-bootstrap';
+import { Component } from 'react'
 import Container from 'react-bootstrap/esm/Container';
 import SingleBook from './SingleBook';
 
@@ -8,11 +8,12 @@ class AllTheBooks extends Component {
         return (
             <Container className='px-0'>
                 <Row className='mx-1 justify-content-center'>
-                    {this.props.FantasyBooks.map(({asin, title, img, price, category}) => {
+                    {this.props.FantasyBooks.map(({ asin, title, img, price, category }) => {
                         return (
                             <SingleBook key={asin} title={title} img={img} price={price} category={category}></SingleBook>
-                                )})}
-                </Row>  
+                        )
+                    })}
+                </Row>
             </Container>
         );
     }
